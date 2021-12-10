5390e9836f6ce31488cd8596b1ad66f11351cbc0
Khushi <khushitiwari2603@gmail.com>
2021-12-11 00:20:42 +0530

    heyy

--- Quiz.java
+++ Quiz.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,17 +26,17 @@ public class Quiz extends JFrame{
 	JLabel roll = new JLabel("Enter your roll no. : ") ;
 	JTextField tf2 = new JTextField(25);
 
-	JLabel Q1 = new JLabel("Question001.. 4 - 2 ? ") ;
+	JLabel Q1 = new JLabel("Question 1.. 4 - 2 ? ") ;
 	JRadioButton Two = new JRadioButton("2");
 	JRadioButton Three = new JRadioButton("3");
 	JRadioButton Four = new JRadioButton("4");
 	JRadioButton Five = new JRadioButton("5");
-	JLabel Q2 = new JLabel("Question002.. 99 + 1 ? ") ;
+	JLabel Q2 = new JLabel("Question 2.. 99 + 1 ? ") ;
 	JRadioButton hundred = new JRadioButton("100");
 	JRadioButton thundred = new JRadioButton("200");
 	JRadioButton fifty = new JRadioButton("50");
 	JRadioButton nine = new JRadioButton("9");
-	JLabel Q3 = new JLabel("Question003.. 2 - 2 ? ") ;
+	JLabel Q3 = new JLabel("Question 3.. 2 - 2 ? ") ;
 	JRadioButton One = new JRadioButton("1");
 	JRadioButton Zero = new JRadioButton("0");
 	JRadioButton mOne = new JRadioButton("-1");
@@ -46,15 +47,15 @@ public class Quiz extends JFrame{
 		
 		panelCont.setLayout(cl); 
 
-		name.setFont(new Font("Osward",Font.BOLD,38)); 
-		roll.setFont(new Font("Osward",Font.BOLD,38));
+		name.setFont(new Font("Raleway",Font.BOLD,38)); 
+		roll.setFont(new Font("Raleway",Font.BOLD,38));
 		name.setForeground(Color.decode("#1c0052"));
 		name.setBounds(250, 145, 375, 200);
 		roll.setBounds(250, 225, 375, 200);
 		roll.setForeground(Color.decode("#1c0052"));
-		tf1.setFont(new Font("Arial",Font.BOLD,14));
+		tf1.setFont(new Font("Raleway",Font.BOLD,14));
 		tf1.setBounds(400,235,230,30);
-		tf2.setFont(new Font("Arial",Font.BOLD,14));
+		tf2.setFont(new Font("Raleway",Font.BOLD,14));
 		tf2.setBounds(400,310,230,30);
 
 		
@@ -64,17 +65,50 @@ public class Quiz extends JFrame{
 		
 		panelFirst.add(tf2);
 		panelFirst.add(buttonOne);
+
+		Q1.setFont(new Font("Raleway",Font.BOLD,38));
+		Q1.setForeground(Color.decode("#1c0052"));
+		Two.setFont(new Font("Raleway",Font.BOLD,38));
+		Two.setForeground(Color.decode("#1c0052"));
+		Three.setFont(new Font("Raleway",Font.BOLD,38));
+		Three.setForeground(Color.decode("#1c0052"));
+		Four.setFont(new Font("Raleway",Font.BOLD,38));
+		Four.setForeground(Color.decode("#1c0052"));
+		Five.setFont(new Font("Raleway",Font.BOLD,38));
+		Five.setForeground(Color.decode("#1c0052"));
 		panelSecond.add(Q1);
 		panelSecond.add(Two);
 		panelSecond.add(Three);
 		panelSecond.add(Four);
 		panelSecond.add(Five);
 		
+		Q2.setFont(new Font("Raleway",Font.BOLD,38));
+		Q2.setForeground(Color.decode("#1c0052"));
+		hundred.setFont(new Font("Raleway",Font.BOLD,38));
+		hundred.setForeground(Color.decode("#1c0052"));
+		thundred.setFont(new Font("Raleway",Font.BOLD,38));
+		thundred.setForeground(Color.decode("#1c0052"));
+		fifty.setFont(new Font("Raleway",Font.BOLD,38));
+		fifty.setForeground(Color.decode("#1c0052"));
+		nine.setFont(new Font("Raleway",Font.BOLD,38));
+		nine.setForeground(Color.decode("#1c0052"));
+
 		panelSecond.add(Q2);
 		panelSecond.add(hundred);
 		panelSecond.add(thundred);
 		panelSecond.add(fifty);
 		panelSecond.add(nine);
+
+		Q3.setFont(new Font("Raleway",Font.BOLD,45));
+		Q3.setForeground(Color.decode("#1c0052"));
+		One.setFont(new Font("Raleway",Font.BOLD,38));
+		One.setForeground(Color.decode("#1c0052"));
+		Zero.setFont(new Font("Raleway",Font.BOLD,38));
+		Zero.setForeground(Color.decode("#1c0052"));
+		mOne.setFont(new Font("Raleway",Font.BOLD,38));
+		mOne.setForeground(Color.decode("#1c0052"));
+		mTwo.setFont(new Font("Raleway",Font.BOLD,38));
+		mTwo.setForeground(Color.decode("#1c0052"));
 
 		panelSecond.add(buttonSecond);
 		panelThird.add(Q3);
@@ -126,8 +160,8 @@ public class Quiz extends JFrame{
 				
  			UIManager.put("OptionPane.background",Color.decode("#cdecb6"));
 			UIManager.put("Panel.background",Color.decode("#cdecb6"));
-			
-				JOptionPane.showMessageDialog(tf1, tf1.getText()+ "'s  (" + tf2.getText() + ") Score : "+ marks ); 
+			//UIManager.put("Panel.title", toString() );
+				JOptionPane.showMessageDialog(tf1, tf1.getText()+ "'s  (" + tf2.getText() + ") Score : "+ marks, "Result", JOptionPane.INFORMATION_MESSAGE); 
 			}
 		});
 		
